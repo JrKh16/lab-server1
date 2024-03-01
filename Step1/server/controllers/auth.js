cc815d363c63e80610ef98ee5c215fd3559535bf
JrKh16 <jirawatkhunngean@gmail.com>
2024-03-01 16:50:26 +0700

    change it back :)

--- Step1/server/controllers/auth.js
+++ Step1/server/controllers/auth.js
@@ -1,37 +1,45 @@
 const { response } = require("express");
-const mysql = require('mysql');
+
+let mysql = require('mysql');
 const env = require('../env.js');
 const config = require('../dbconfig.js')[env];
 
 const login = async (req, res = response) => {
   const { email, password } = req.body;
 
+  //----------------------
+
   let dbcon = mysql.createConnection(config);
 
-  const userDetails = "SELECT * FROM users WHERE email = ?";
+  const userDetails = "SELECT * FROM users where email = '" + email + "'";
   console.log(userDetails);
 
-  dbcon.query(userDetails, [email], function (err, result) {
-    if (err) {
-      console.error(err);
-      return res.status(500).json({ message: "Database error" });
-    }
+  dbcon.query(userDetails, function (err, user) {
+    console.log(user);
 
-    console.log(result);
-
-    if (result && result.length > 0) {
-      const user = result[0];
-
-      if (password !== user.password) {
-        return res.status(400).json({ message: "User / Password are incorrect" });
+    if (user.length > 0) {
+      if (password !== user[0].password) {
+        return res.status(400).json({
+          msg: "User / Password are incorrect",
+        });
       }
 
-      res.status(200).json({ user });
+      res.status(200).json({ user })
+
+      /*
+            res.json({
+              name: "Test User",
+              token: "A JWT token to keep the user logged in.",
+              msg: "Successful login",
+            });
+      */
+
     } else {
       // User not found
-      return res.status(401).json({ message: "User not found!" });
+      return res.status(401).json({ message: "User not found !" })
     }
-  });
+  })
+
 };
 
 module.exports = {
